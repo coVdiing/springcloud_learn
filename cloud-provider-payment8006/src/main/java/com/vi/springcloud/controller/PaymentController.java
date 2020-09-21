5eab4a2a7ad60b84ef70088264ea05d12e4128ff
coVdiing <113714806@qq.com>
2020-09-21 13:46:26 +0800

    新增payment8006模块,作为服务提供者注册到consul

---
+++ cloud-provider-payment8006/src/main/java/com/vi/springcloud/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.vi.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/consul")
+    public String paymentConsul() {
+        return "spring cloud with consul,port =>" + serverPort + ", " + UUID.randomUUID().toString();
+    }
+}
